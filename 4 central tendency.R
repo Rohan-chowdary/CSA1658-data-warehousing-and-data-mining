6b3e5835525800e7b787bf80b201e91b528345a6
Rohan-chowdary <113493120+Rohan-chowdary@users.noreply.github.com>
2022-10-08 21:07:18 +0530

    Add files via upload

---
+++ 4 central tendency.R
@@ -0,0 +1,18 @@
+# Defining vector
+x <- c(3, 7, 5, 13, 20, 23, 39, 23, 40, 23, 14, 12, 56, 23)
+# Print mean
+print (mean (x))
+# Defining vector
+x <- c(1, 5, 9, 19, 25)
+# Print Geometric Mean
+print (prod (x) ^ (1 / length (x)))
+ # Defining vector
+x <- c(1, 5, 8, 10)
+# Print Harmonic mean 
+print (1 / mean (1 / x))
+# Defining vector
+
+"x <- c(3, 7, 5, 13, 20, 23, 39,
+        23, 40, 23. 14. 12. 56, 23)"
+# Print median
+median (x)
